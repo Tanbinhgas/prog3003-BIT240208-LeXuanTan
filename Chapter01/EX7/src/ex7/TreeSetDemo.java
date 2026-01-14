3625c4d218c60f394bda4d5b9ad6efb749e8448b
Tanbinhgas <lexuantan2k@gmail.com>
2026-01-14 18:32:29 +0700

    bai tap chuong 1

--- Chapter01/EX7/src/ex7/TreeSetDemo.java
+++ Chapter01/EX7/src/ex7/TreeSetDemo.java
@@ -8,13 +8,35 @@ package ex7;
  *
  * @author tanbinhgas
  */
+import java.util.TreeSet;
+
 public class TreeSetDemo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        TreeSet<String> names = new TreeSet<>();
+        
+        names.add("John");
+        names.add("Alice");
+        names.add("Zack");
+        names.add("Bob");
+        
+        System.out.println("Danh sach sau khi them:");
+        System.out.println(names);
+        
+        System.out.println("\nDanh sach chi tiet:");
+        for (String name : names) {
+            System.out.println("- " + name);
+        }
+        
+        if (!names.isEmpty()) {
+            String smallest = names.first();
+            String largest  = names.last();
+            
+            System.out.println("\nPhan tu đau tien: " + smallest);
+            System.out.println("Phan tu cuoi cung:  " + largest);
+        } else {
+            System.out.println("TreeSet rỗng!");
+        }
     }
-    
 }
